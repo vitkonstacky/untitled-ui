8aa1514a9e9aab1ab6af4ccad98f95b73db7e266
Vít Konštacký <vit.konstacky@partory.com>
2025-10-05 17:03:51 +0200

    Fixed serve sometimes not loading anything before first refresh

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,65 @@ const dist = path.resolve(__dirname + "/dist");
  * Compilation
  */
 
-gulp.task("serve", async () => {
+gulp.task("build", (done) => {
+
+  const version = process.argv.includes("--alpine") ? "alpine" : "pure";
+
+  del.deleteSync(dist);
+
+  gulp.src(src + "/html/" + version + "/index.html", {
+    encoding: false,
+  }).pipe(
+    gulp.dest(dist),
+  );
+
+  gulp.src(src + "/favicon.png", {
+    encoding: false,
+  }).pipe(
+    gulp.dest(dist),
+  );
+
+  gulp.src(src + "/img/**/*", {
+    encoding: false,
+  }).pipe(
+    gulp.dest(dist + "/img"),
+  );
+
+  gulp.src(src + "/scss/index.scss").pipe(
+    sass({
+      style: "compressed",
+    }).on("error", sass.logError),
+  ).pipe(
+    concat("app.css"),
+  ).pipe(
+    gulp.dest(dist + "/css"),
+  );
+
+  gulp.src(src + "/js/" + version + "/index.js",).pipe(
+    uglify({
+      mangle: {
+        toplevel: true,
+      },
+    }),
+  ).pipe(
+    rename("app.js"),
+  ).pipe(
+    gulp.dest(dist + "/js"),
+  );
+
+  done();
+
+});
+
+gulp.task("serve", gulp.series("build", () => {
+
+  gulp.watch(src, gulp.series("build", (done) => {
+
+    browser.reload();
+
+    done();
+
+  }));
 
   browser.init({
 
@@ -42,74 +100,4 @@ gulp.task("serve", async () => {
 
   });
 
-  gulp.watch(src, {
-
-    ignoreInitial: false,
-
-  }, async (done) => {
-
-    const version = process.argv.includes("--alpine") ? "alpine" : "pure";
-
-    del.deleteSync(dist + "/*");
-
-    await gulp.src(src + "/html/" + version + "/index.html", {
-
-      encoding: false,
-
-    }).pipe(
-      gulp.dest(dist),
-    );
-
-    await gulp.src(src + "/favicon.png", {
-
-      encoding: false,
-
-    }).pipe(
-      gulp.dest(dist),
-    );
-
-    await gulp.src(src + "/img/**/*", {
-
-      encoding: false,
-
-    }).pipe(
-      gulp.dest(dist + "/img"),
-    );
-
-    await gulp.src(src + "/scss/index.scss").pipe(
-      sass({
-
-        style: "compressed",
-
-      }).on("error", sass.logError),
-    ).pipe(
-      concat("app.css"),
-    ).pipe(
-      gulp.dest(dist + "/css"),
-    );
-
-    await gulp.src(src + "/js/" + version + "/index.js").pipe(
-      uglify({
-
-        mangle: {
-
-          toplevel: true,
-
-        }
-
-      }),
-    ).pipe(
-      rename("app.js"),
-    ).pipe(
-      gulp.dest(dist + "/js"),
-    );
-
-    browser.reload();
-
-    done();
-
-  });
-
-  browser.reload();
-
-});
+}));
--- readme.md
+++ readme.md
@@ -10,13 +10,19 @@ This repository serves only as showcase of skills for potential employment.
 - Node `22.20.0`
 - NPM `11.6.1`
 
-## How To Run
+## How to run
 
 - Clone this repository
 - Run `npm install`
 - Run `npm run serve:pure` or `npm run serve:alpine` depending on if you want to see result achieved with *pure javascript* or *alpine.js*
-- Your preferred browser should automatically open at http://localhost:3000 (if you see a blank page on initial load, simply refresh the page and everything should work)
+- Your preferred browser should automatically open at http://localhost:3000
+
+## How to build 
+
+- Clone this repository
+- Run `npm install`
+- Run `npm run build:pure` or `npm run build:alpine` depending on if you want to see result achieved with *pure javascript* or *alpine.js*
 
 ## Known issues
 
-- The `Products` navigation item has dropdown icon, but no content, as there was none in the figma design file or I could not find it
+- The `Products` navigation item has dropdown icon, but no content, as there was none in the figma design file, or I could not find it
